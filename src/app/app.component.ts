e75813ce06a195d0f948c64b2d901e5194698670
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-09-27 15:54:05 +0200

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslationService } from './services/translation/translation.service';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'lux';
+
+	constructor(private translation: TranslationService) {	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AlertService } from './services/alert/alert.service';
+import { TranslationService } from './services/translation/translation.service';
+
 import { AppComponent } from './app.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
+const routes: Routes = [];
+
 @NgModule({
-	declarations: [ AppComponent ],
-	bootstrap: [ AppComponent ],
-	imports: [
-		AppRoutingModule,
-		BrowserModule
+	declarations: [
+		AppComponent
 	],
-	providers: [],
+	bootstrap: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		RouterModule.forRoot(routes),
+		TranslateModule.forRoot({
+			loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] }
+		})
+	],
+	providers: [
+		AlertService,
+        TranslationService
+	]
 })
 export class AppModule { }
