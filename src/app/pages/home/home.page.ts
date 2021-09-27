687916e47a423c0efe764d7f441b22e1e84b2a19
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-09-27 21:49:45 +0200

    home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,4 +4,32 @@ import { Component } from '@angular/core';
 	templateUrl: './home.page.html',
 	styleUrls: ['./home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+	public foundations: string[] = [
+		"El sombriure dels nens",
+		"Banc d'aliments",
+		"LINFACAT",
+		"AECC",
+		"El caliu",
+		"Fundació Sant Vicenç de Paül",
+		"Fundació amics de la gent gran",
+		"Live! Hoy empieza una nueva vida",
+		"Associació d'hemoglubinúria paroxisticanocturna",
+		"Centre de faunasalvatge de la masia camadoca (Berguedà)",
+		"Save the children"
+	];
+
+	public organizations: string[] = [
+		"hotel-subur.png",
+		"bcn-alberg.jpg",
+		"xarxa-evolució.png",
+		"xanas.jpg",
+		"espai-ferrer.png",
+		"vall-formosa.png",
+		"generalitat-catalunya.jpg",
+		"bcn-ajuntament.jpg",
+		"laCaixa.jpg",
+		"bcn-diputació.jpg"
+	];
+}
