12b3e39b908e1a0ba6e60d4a17f16eb4036f8861
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-09-28 10:51:22 +0200

    header init

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared.module";
 
 import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
 	declarations: [
-		FooterComponent
+		FooterComponent,
+		HeaderComponent
 	],
 	exports: [
-		FooterComponent
+		FooterComponent,
+		HeaderComponent
 	],
 	imports: [ SharedModule ]
 })
