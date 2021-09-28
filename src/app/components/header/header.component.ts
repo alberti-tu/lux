966c15773768d19c0989148cb744947e191f4913
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-09-28 11:51:21 +0200

    color theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+	public links: string[] = [
+		// 'shows',
+		'home',
+		'about'
+	];
+
+}
