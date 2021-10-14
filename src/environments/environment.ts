66f637613984cad7371667842069dd1bbfbe7379
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-10-14 15:59:24 +0200

    mozila date format

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -80,9 +80,9 @@ export const environment: Configuration = {
 	],
 	reports: [ "2018", "2017", "2016", "2015" ],
 	shows: [
-		{ name: "Test 1", address: "Les Rambles 4, Barcelona", date: "16:00 11-1-2021", price: "5€", url: "https://www.google.es" },
-		{ name: "Test 2", address: "Plaza Catalunya, Barcelona", date: "21:00 12-8-2021", price: "15€", url: "https://www.facebook.com" },
-		{ name: "Test 3", address: "Plaza España, Sevilla", date: "18:30 3-5-2022", price: undefined, url: undefined },
-		{ name: "Test 4", address: "Parque del Retiro, Madrid", date: "19:00 9-12-2022", price: "10€", url: undefined }
+		{ name: "Test 1", address: "Les Rambles 4, Barcelona", date: "2021-11-1 16:00", price: "5€", url: "https://www.google.es" },
+		{ name: "Test 2", address: "Plaza Catalunya, Barcelona", date: "2021-12-8 21:00", price: "15€", url: "https://www.facebook.com" },
+		{ name: "Test 3", address: "Plaza España, Sevilla", date: "2022-5-3 18:30", price: undefined, url: undefined },
+		{ name: "Test 4", address: "Parque del Retiro, Madrid", date: "2022-12-9 19:00", price: "10€", url: undefined }
 	]
 };
