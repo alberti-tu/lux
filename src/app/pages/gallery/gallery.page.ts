86bbba32e20a96230b1a01f4c792f8ae118db0f0
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-10-19 13:48:40 +0200

    selected disc

--- src/app/pages/gallery/gallery.page.ts
+++ src/app/pages/gallery/gallery.page.ts
@@ -10,12 +10,15 @@ import { environment } from 'src/environments/environment';
 export class GalleryPage {
 
 	public discography: Disc[];
+	public discIndex: number;
 
 	constructor(private router: Router) {
 		this.discography = environment.discography.sort((a, b) => (a.year && b.year ? a.year - b.year : 0));
 	}
 
-	public open(item: Disc): void {
-		this.router.navigateByUrl('/gallery/player', { state: item });
+	public open(item: Disc, index:  number): void {
+		if (this.discIndex == index) {
+			this.router.navigateByUrl('/gallery/player', { state: item });
+		}
 	}
 }
