2df6aaee39c9d725faf0f62f86813339c9a55faf
MarisaLomon <haylinasalo@gmail.com>
2022-04-03 17:06:42 +0200

    ios dates fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -80,9 +80,9 @@ export const environment: Configuration = {
 	],
 	reports: [ "2018", "2017", "2016", "2015" ],
 	shows: [
-		{ name: "Test 1", address: "Les Rambles 4, Barcelona", date: "2021-11-1 16:00", price: "5€", url: "https://www.google.es" },
-		{ name: "Test 2", address: "Plaza Catalunya, Barcelona", date: "2021-12-8 21:00", price: "15€", url: undefined },
-		{ name: "Test 3", address: "Plaza España, Sevilla", date: "2022-5-3 18:30", price: undefined, url: "https://www.google.es" },
-		{ name: "Test 4", address: "Parque del Retiro, Madrid", date: "2022-12-9 19:00", price: undefined, url: undefined }
+		{ name: "Test 1", address: "Les Rambles 4, Barcelona", date: "2021/11/1 16:00", price: "5€", url: "https://www.google.es" },
+		{ name: "Test 2", address: "Plaza Catalunya, Barcelona", date: "2021/12/8 21:00", price: "15€", url: undefined },
+		{ name: "Test 3", address: "Plaza España, Sevilla", date: "2022/5/3 18:30", price: undefined, url: "https://www.google.es" },
+		{ name: "Test 4", address: "Parque del Retiro, Madrid", date: "2022/12/9 19:00", price: undefined, url: undefined }
 	]
 };
